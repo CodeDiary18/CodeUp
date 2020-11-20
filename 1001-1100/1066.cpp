c1a3942debdef17184e6b12f975818f420ddf136
CodeDiary18 <74449232+CodeDiary18@users.noreply.github.com>
2020-11-21 01:20:40 +0900

    Create 1066.cpp

---
+++ 1001-1100/1066.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main(){
+    int a,b,c;
+    std::cin>>a>>b>>c;
+    if(a%2==0) std::cout<<"even\n";
+    else std::cout<<"odd\n";
+    if(b%2==0) std::cout<<"even\n";
+    else std::cout<<"odd\n";
+    if(c%2==0) std::cout<<"even\n";
+    else std::cout<<"odd\n";
+    return 0;
+}
