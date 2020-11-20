feaded075c14a69cb091ce5101f5d6fed3b371c5
CodeDiary18 <74449232+CodeDiary18@users.noreply.github.com>
2020-11-21 01:18:43 +0900

    Create 1065.cpp

---
+++ 1001-1100/1065.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(){
+    int a,b,c;
+    std::cin>>a>>b>>c;
+    if(a%2==0){
+        std::cout<<a<<"\n";
+    }
+    if(b%2==0){
+        std::cout<<b<<"\n";
+    }
+    if(c%2==0){
+        std::cout<<c<<"\n";
+    }
+    return 0;
+}
